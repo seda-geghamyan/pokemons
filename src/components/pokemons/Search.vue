39040b2b6b5232db5411c015b2ba07719a55eed2
Seda Geghamyan <seda.geghamyan@exio.tech>
2022-10-13 22:26:18 +0400

    Search

---
+++ src/components/pokemons/Search.vue
@@ -0,0 +1,18 @@
+<template>
+  <input
+    v-model="value"
+    type="text"
+    placeholder="Search . . . "
+    class="form-control"
+    @input="$emit('update:value', value)"
+  />
+</template>
+<script>
+export default {
+  props: {
+    value: {
+      type: String,
+    },
+  },
+};
+</script>
--- src/views/Pokemons/index.vue
+++ src/views/Pokemons/index.vue
@@ -1,10 +1,20 @@
 <template>
   <div v-if="pokemons.length">
-    <h1 class="text-center text-light mb-4">Pokemons</h1>
     <div class="row">
+      <div class="col-md-6 col-lg-4 mb-4">
+        <pokemons-search v-model:value="search" />
+      </div>
+      <div class="col-12">
+        <h1 class="text-center text-light mb-4">Pokemons</h1>
+        <div v-if="!pokemonsList.length">
+          <p class="text-center fs-5 py-5">
+            Try find your pokemon in another pages.
+          </p>
+        </div>
+      </div>
       <div
         class="col-sm-6 col-md-4 col-lg-3 mb-4"
-        v-for="pokemon in pokemons"
+        v-for="pokemon in pokemonsList"
         :key="pokemon.name"
       >
         <a
@@ -36,9 +46,10 @@
 </template>
 <script>
 import { mapGetters, mapActions } from "vuex";
-import AppCard from "@/components/app/Card.vue";
-import AppLoader from "@/components/app/Loader.vue";
+import AppCard from "@/components/app/Card";
+import AppLoader from "@/components/app/Loader";
 import VPagination from "@hennge/vue3-pagination";
+import PokemonsSearch from "@/components/pokemons/Search";
 
 export default {
   name: "PokemonsListView",
@@ -47,12 +58,14 @@ export default {
     AppCard,
     AppLoader,
     VPagination,
+    PokemonsSearch,
   },
 
   data() {
     return {
       page: 1,
       offset: 0,
+      search: "",
     };
   },
 
@@ -76,6 +89,12 @@ export default {
     ...mapGetters({
       pokemons: "pokemons/allPokemons",
     }),
+
+    pokemonsList() {
+      return this.search.length >= 3
+        ? this.pokemons.filter((item) => item.name.includes(this.search))
+        : this.pokemons;
+    },
   },
 };
 </script>
